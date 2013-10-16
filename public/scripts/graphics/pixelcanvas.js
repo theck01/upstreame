405ae538e89d0aea3e0ae0607944c49f6af97876
Tyler Heck <tyler.heck@gmail.com>
2013-10-15 20:39:07 -0500

    setup pixel*.js for prototypal inheritance

--- public/scripts/graphics/pixelcanvas.js
+++ public/scripts/graphics/pixelcanvas.js
@@ -1,6 +1,28 @@
 define(["jquery", "underscore", "graphics/color"],
   function ($, _, Color) {
 
+    // function creates a 2D array with given dimensions containin an RGB
+    // string at each location in the array
+    //
+    // Arguments:
+    //   width: width of the pixel grid
+    //   height: height of the pixel grid
+    //   color: default color of pixels not drawn to, "#RRGGBB" string
+    function makePixelGrid(width, height, color) {
+      var ary = [];
+      var i, j;
+
+      for(i=0; i<width; i++){
+        ary[i] = [];
+        for(j=0; j<height; j++){
+          ary[i][j] = _.clone(color);
+        }
+      }
+      
+      return ary;
+    }
+
+
     // PixelCanvas object abstracts the HTML canvas object and exposes an API to
     // draw meta-pixels on the canvas.
     //
@@ -10,126 +32,107 @@ define(["jquery", "underscore", "graphics/color"],
     //   backgroundColor: default color of pixels not drawn to, "#RRGGBB" string
     //   canvasID: css selector style id of the canvas on the page
     var PixelCanvas = function (width, height, backgroundColor, canvasID) {
-
-      var dim = { width: width, height: height };
-      var backgroundHexColor = Color.sanitize(backgroundColor);
-      var pixelBuffer = makePixelGrid(dim.width, dim.height);
-      var htmlCanvas = $(canvasID)[0];
+      this.dim = { width: width, height: height };
+      this.backgroundColor = backgroundColor;
+      this.pixelBuffer = makePixelGrid(this.dim.width, this.dim.height,
+                                       this.backgroundColor);
+      this.htmlCanvas = $(canvasID)[0];
+    };
       
       
-      // getPixel returns the color of the meta-pixel at location x,y
-      //
-      // Arguments:
-      //   x: x position of the pixel in the grid from left most (0) to right
-      //      most (+ width)
-      //   y: y position of the pixel in the grid from top most (0) to bottom
-      //      most (+ height)
-      //
-      // Returns:
-      //   A color hexadecimal string in the format "#RRGGBB"
-      this.getPixel = function (x, y) {
-        if(x > dim.width || x < 0 || y > dim.height || y < 0)
-          return "#000000";
-        return pixelBuffer[x][y];
-      };
+    // getPixel returns the color of the meta-pixel at location x,y
+    //
+    // Arguments:
+    //   x: x position of the pixel in the grid from left most (0) to right
+    //      most (+ width)
+    //   y: y position of the pixel in the grid from top most (0) to bottom
+    //      most (+ height)
+    //
+    // Returns:
+    //   A color hexadecimal string in the format "#RRGGBB"
+    PixelCanvas.prototype.getPixel = function (x, y) {
+      if(x > this.dim.width || x < 0 || y > this.dim.height || y < 0)
+        return "#000000";
+      return this.pixelBuffer[x][y];
+    };
 
 
-      // function creates a 2D array with given dimensions containin an RGB
-      // string at each location in the array
-      //
-      // Arguments:
-      //   width: width of the pixel grid
-      //   height: height of the pixel grid
-      function makePixelGrid(width, height) {
-        var ary = [];
-        var i, j;
+    // paint draws the pixel buffer to the HTML canvas and resets the buffer
+    // to contain all white pixels
+    PixelCanvas.prototype.paint = function () {
 
-        for(i=0; i<width; i++){
-          ary[i] = [];
-          for(j=0; j<height; j++){
-            ary[i][j] = backgroundHexColor;
-          }
-        }
-        
-        return ary;
-      }
+      var context = this.htmlCanvas.getContext("2d");
+      var sparams = this.screenParams(this.dim.width, this.dim.height);
+      var x, y;
 
+      // clear the canvas
+      context.clearRect(0,0,this.htmlCanvas.width,this.htmlCanvas.height);
 
-      // paint draws the pixel buffer to the HTML canvas and resets the buffer
-      // to contain all white pixels
-      this.paint = function () {
-
-        var context = htmlCanvas.getContext("2d");
-        var sparams = this.screenParams(dim.width, dim.height);
-        var x, y;
-
-        // clear the canvas
-        context.clearRect(0,0,htmlCanvas.width,htmlCanvas.height);
-
-        // draw each pixel individually
-        _.each(pixelBuffer, function(column, i){
-          _.each(column, function(color, j){
-            x = sparams.xoffset + i*sparams.pixelSize;
-            y = sparams.yoffset + j*sparams.pixelSize;
-            context.fillStyle = color;
-            context.fillRect(x,y,sparams.pixelSize,sparams.pixelSize);
-          });
+      // draw each pixel individually
+      _.each(this.pixelBuffer, function(column, i){
+        _.each(column, function(color, j){
+          x = sparams.xoffset + i*sparams.pixelSize;
+          y = sparams.yoffset + j*sparams.pixelSize;
+          context.fillStyle = color;
+          context.fillRect(x,y,sparams.pixelSize,sparams.pixelSize);
         });
+      });
 
-        // reset grid to all white 
-        pixelBuffer = makePixelGrid(dim.width, dim.height);
-      };
-
-
-      // screenParams measures current canvas dimensions and returns an object 
-      // with the fields:
-      //
-      // Arguments:
-      //   pixelWidth: width of the pixelCanvas in meta-pixels
-      //   pixelHeight: height of the pixelCanvas in meta-pixels
-      //
-      // Return fields:
-      //   pixelSize: meta-pixel width and height in screen pixels
-      //   xoffset: x offset in screen pixels of the left most pixels from the
-      //            left edge of the canvas
-      //   yoffset: yoffset in screen pixels of the top most pixels from the
-      //            top most edge of the canvas
-      this.screenParams = function (pixelWidth, pixelHeight) {
-        var retval = {};
-
-        var height = htmlCanvas.height;
-        var width = htmlCanvas.width;
-        
-        var xfactor = Math.floor(width/pixelWidth);
-        var yfactor = Math.floor(height/pixelHeight);
-
-        // meta-pixel dimensions determined by the smallest screen pixel to 
-        // meta-pixel ratio, so that all pixels will fit on screen
-        retval.pixelSize = xfactor < yfactor ? xfactor : yfactor;
-
-        // compute offsets using computed pixelSize and number of pixels in each
-        // dimension so that the canvas is centered
-        retval.xoffset = Math.floor((width - retval.pixelSize*pixelWidth)/2);
-        retval.yoffset = Math.floor((height - retval.pixelSize*pixelHeight)/2);
-
-        return retval;
-      };
+      // reset grid to all white 
+      this.pixelBuffer = makePixelGrid(this.dim.width, this.dim.height,
+                                       this.backgroundColor);
+    };
 
 
-      // setPixel colors in the meta-pixel at location (x,y) with given color 
-      // within the meta-pixel buffer
-      //
-      // Arguments:
-      //   x: x position of the pixel in the grid from left most (0) to right
-      //      most (+ width)
-      //   y: y position of the pixel in the grid from top most (0) to bottom
-      //      most (+ height)
-      //   color: A hexadecimal string in the format "#RRGGBB"
-      this.setPixel = function (x, y, color) {
-        // dont write to buffer if location is outside canvas bounds
-        if(x >= dim.width || x < 0 || y >= dim.height || y < 0) return;
-        pixelBuffer[x][y] = Color.sanitize(color);
-      };
+    // screenParams measures current canvas dimensions and returns an object 
+    // with the fields:
+    //
+    // Arguments:
+    //   pixelWidth: width of the pixelCanvas in meta-pixels
+    //   pixelHeight: height of the pixelCanvas in meta-pixels
+    //
+    // Return fields:
+    //   pixelSize: meta-pixel width and height in screen pixels
+    //   xoffset: x offset in screen pixels of the left most pixels from the
+    //            left edge of the canvas
+    //   yoffset: yoffset in screen pixels of the top most pixels from the
+    //            top most edge of the canvas
+    PixelCanvas.prototype.screenParams = function (pixelWidth, pixelHeight) {
+      var retval = {};
+
+      var height = this.htmlCanvas.height;
+      var width = this.htmlCanvas.width;
+      
+      var xfactor = Math.floor(width/pixelWidth);
+      var yfactor = Math.floor(height/pixelHeight);
+
+      // meta-pixel dimensions determined by the smallest screen pixel to 
+      // meta-pixel ratio, so that all pixels will fit on screen
+      retval.pixelSize = xfactor < yfactor ? xfactor : yfactor;
+
+      // compute offsets using computed pixelSize and number of pixels in each
+      // dimension so that the canvas is centered
+      retval.xoffset = Math.floor((width - retval.pixelSize*pixelWidth)/2);
+      retval.yoffset = Math.floor((height - retval.pixelSize*pixelHeight)/2);
+
+      return retval;
+    };
+
+
+    // setPixel colors in the meta-pixel at location (x,y) with given color 
+    // within the meta-pixel buffer
+    //
+    // Arguments:
+    //   x: x position of the pixel in the grid from left most (0) to right
+    //      most (+ width)
+    //   y: y position of the pixel in the grid from top most (0) to bottom
+    //      most (+ height)
+    //   color: A hexadecimal string in the format "#RRGGBB"
+    PixelCanvas.prototype.setPixel = function (x, y, color) {
+      // dont write to buffer if location is outside canvas bounds
+      if(x >= this.dim.width || x < 0 || y >= this.dim.height || y < 0)
+        return;
+      this.pixelBuffer[x][y] = Color.sanitize(color);
     };
 
     return PixelCanvas;
--- public/scripts/interface/pixelcolorer.js
+++ public/scripts/interface/pixelcolorer.js
@@ -10,272 +10,279 @@ define(["jquery", "underscore", "graphics/pixelcanvas", "graphics/color"],
     //   height: height of the pixel canvas in meta-pixels
     //   canvasID: css selector style id of the canvas on the page
     var PixelColorer = function (width, height, canvasID) {
-
-      var $htmlCanvas = $(canvasID);
-      var action = "set";
-      var backgroundColor = "#FFFFFF";
-      var currentColor = "#000000";
-      var mouseDown = false;
-      var pCanvas = new PixelCanvas(width, height, backgroundColor, canvasID);
-      var pixelWidth = width;
-      var pixelHeight = height;
-      var pixels = [];
-      var mouseMoveAction = function () {};
-      var showGrid = true;
       var that = this;
+
+      this.$htmlCanvas = $(canvasID);
+      this.action = "set";
+      this.backgroundColor = "#FFFFFF";
+      this.canvasID = canvasID;
+      this.currentColor = "#000000";
+      this.mouseDown = false;
+      this.pCanvas = new PixelCanvas(width, height, this.backgroundColor,
+                                     canvasID);
+      this.pixelWidth = width;
+      this.pixelHeight = height;
+      this.pixels = [];
+      this.mouseMoveAction = function () {};
+      this.showGrid = true;
 
 
       // on mouseup or mouseleave set mouseDown to false
-      $htmlCanvas.on("mouseup mouseleave", function () {
-        mouseDown = false;
+      this.$htmlCanvas.on("mouseup mouseleave", function () {
+        that.mouseDown = false;
       });
 
 
       // set up mouse listener for down and movement events
-      $htmlCanvas.on("mousedown mousemove", function (e) {
+      this.$htmlCanvas.on("mousedown mousemove", function (e) {
 
-        if(e.type === "mousedown") mouseDown = true;
+        if(e.type === "mousedown") that.mouseDown = true;
 
         // if user is not currently clicking, do nothing
-        if(!mouseDown) return;
+        if(!that.mouseDown) return;
 
-        var canvasOffset = $htmlCanvas.offset();
+        var canvasOffset = that.$htmlCanvas.offset();
         var relx = e.pageX - canvasOffset.left;
         var rely = e.pageY - canvasOffset.top;
 
-        var sparams = pCanvas.screenParams(pixelWidth, pixelHeight);
+        var sparams = that.pCanvas.screenParams(that.pixelWidth,
+                                                that.pixelHeight);
 
         var x = Math.floor((relx - sparams.xoffset)/sparams.pixelSize);
         var y = Math.floor((rely - sparams.yoffset)/sparams.pixelSize);
 
-        var matchingPixel = _.find(pixels, function (p) {
+        var matchingPixel = _.find(that.pixels, function (p) {
           return p.x === x && p.y === y;
         });
 
         // if click was outside pixel region do nothing
-        if(x > pixelWidth || x < 0 || y > pixelHeight || y < 0) return;
+        if(x > that.pixelWidth || x < 0 || y > that.pixelHeight || y < 0)
+          return;
 
-        if(action === "set"){
+        if(that.action === "set"){
           if(matchingPixel){
-            if(matchingPixel.color !== currentColor){
-              matchingPixel.color = currentColor;
+            if(matchingPixel.color !== that.currentColor){
+              matchingPixel.color = that.currentColor;
               that.paint();
             }
           }
           else{
-            pixels.push({ x: x, y: y, color: currentColor });
+            that.pixels.push({ x: x, y: y, color: that.currentColor });
             that.paint();
           }
         }
-        else if(action === "get"){
+        else if(that.action === "get"){
           if(matchingPixel)
-            currentColor = matchingPixel.color;
+            that.currentColor = matchingPixel.color;
           else
-            currentColor = _.clone(pCanvas.getPixel(x, y));
+            that.currentColor = _.clone(that.pCanvas.getPixel(x, y));
         }
-        else if(action === "clear"){
-          pixels = _.reject(pixels, function (p) {
+        else if(that.action === "clear"){
+          that.pixels = _.reject(that.pixels, function (p) {
             return p.x === x && p.y === y;
           });
           that.paint();
         }
 
-        mouseMoveAction(e);
+        that.mouseMoveAction(e);
+      });
+    };
+
+
+    // clearCanvas reverts all pixels on the PixelCanvas to their default
+    // color
+    PixelColorer.prototype.clearCanvas = function () {
+      this.pixels = [];
+      this.paint();
+    };
+
+
+    // exportPixels generates a JSON string of all meta-pixels set on the
+    // canvas, with additional meta-data about minimum canvas size required to
+    // display the image
+    //
+    // Returns:
+    //   A JSON string representing an object with the fields:
+    //   pixels: An array of objects with x, y, and color fields
+    //   imageWidth: The minimum width of a PixelCanvas required to show the
+    //               complete image
+    //   imageHeight: The minimum height of a PixelCanvas required to show the
+    //                complete image
+    //   center: An object with x and y fields for the center of the image
+    PixelColorer.prototype.exportImage = function () {
+      var image = {};
+      var xvalues = _.map(this.pixels, function (p) {
+        return p.x;
+      });
+      var yvalues = _.map(this.pixels, function (p) {
+        return p.y;
       });
 
+      var xRange = _.reduce(xvalues, function(memo, x) {
+        if(memo[0] > x) memo[0] = x;
+        if(memo[1] < x) memo[1] = x;
+        return memo;
+      }, [Infinity, -Infinity]);
 
-      // clearCanvas reverts all pixels on the PixelCanvas to their default
-      // color
-      this.clearCanvas = function () {
-        pixels = [];
-        this.paint();
-      };
+      var yRange = _.reduce(yvalues, function(memo, y) {
+        if(memo[0] > y) memo[0] = y;
+        if(memo[1] < y) memo[1] = y;
+        return memo;
+      }, [Infinity, -Infinity]);
 
-
-      // exportPixels generates a JSON string of all meta-pixels set on the
-      // canvas, with additional meta-data about minimum canvas size required to
-      // display the image
-      //
-      // Returns:
-      //   A JSON string representing an object with the fields:
-      //   pixels: An array of objects with x, y, and color fields
-      //   imageWidth: The minimum width of a PixelCanvas required to show the
-      //               complete image
-      //   imageHeight: The minimum height of a PixelCanvas required to show the
-      //                complete image
-      //   center: An object with x and y fields for the center of the image
-      this.exportImage = function () {
-        var image = {};
-        var xvalues = _.map(pixels, function (p) {
-          return p.x;
-        });
-        var yvalues = _.map(pixels, function (p) {
-          return p.y;
-        });
-
-        var xRange = _.reduce(xvalues, function(memo, x) {
-          if(memo[0] > x) memo[0] = x;
-          if(memo[1] < x) memo[1] = x;
-          return memo;
-        }, [Infinity, -Infinity]);
-
-        var yRange = _.reduce(yvalues, function(memo, y) {
-          if(memo[0] > y) memo[0] = y;
-          if(memo[1] < y) memo[1] = y;
-          return memo;
-        }, [Infinity, -Infinity]);
-
-        var imageWidth = xRange[1] - xRange[0] + 1;
-        var imageHeight = yRange[1] - yRange[0] + 1;
+      var imageWidth = xRange[1] - xRange[0] + 1;
+      var imageHeight = yRange[1] - yRange[0] + 1;
 
 
-        image.pixels = _.filter(pixels, function (p) {
-          return p.x >=0 && p.x < pixelWidth && p.y >= 0 && p.y < pixelHeight;
-        });
-        image.center = { x: Math.floor(imageWidth/2) + xRange[0],
-                         y: Math.floor(imageHeight/2) + yRange[0] };
+      image.pixels = _.filter(this.pixels, function (p) {
+        return p.x >=0 && p.x < this.pixelWidth && p.y >= 0 &&
+               p.y < this.pixelHeight;
+      }, this);
+      image.center = { x: Math.floor(imageWidth/2) + xRange[0],
+                       y: Math.floor(imageHeight/2) + yRange[0] };
 
-        return image;
-      };
-
-
-      // getBackgroundColor returns the current color that will be set to pixels
-      // that have not been clicked on
-      //
-      // Returns:
-      //   A color hexadecimal string in the form "#RRGGBB"
-      this.getBackgroundColor = function () {
-        return backgroundColor;
-      };
+      return image;
+    };
 
 
-      // getColor returns the current color that will be set to pixels when
-      // clicked on
-      //
-      // Returns:
-      //   A color hexadecimal string in the form "#RRGGBB"
-      this.getColor = function () {
-        return currentColor;
-      };
+    // getBackgroundColor returns the current color that will be set to pixels
+    // that have not been clicked on
+    //
+    // Returns:
+    //   A color hexadecimal string in the form "#RRGGBB"
+    PixelColorer.prototype.getBackgroundColor = function () {
+      return this.backgroundColor;
+    };
 
 
-      // importImage loads a pixel array as the current image
-      this.importImage = function (pixelAry) {
-        pixels = _.map(pixelAry, function (p) {
-          return _.pick(p, ["x", "y", "color"]);
-        });
-        this.paint();
-      };
+    // getColor returns the current color that will be set to pixels when
+    // clicked on
+    //
+    // Returns:
+    //   A color hexadecimal string in the form "#RRGGBB"
+    PixelColorer.prototype.getColor = function () {
+      return this.currentColor;
+    };
 
 
-      // paint writes all stored pixels to the PixelCanvas and calls the
-      // PixelCanvas" paint method
-      this.paint = function () {
-        var context = $htmlCanvas[0].getContext("2d");
-        var i = 0;
-        var sparams = pCanvas.screenParams(pixelWidth, pixelHeight);
+    // importImage loads a pixel array as the current image
+    PixelColorer.prototype.importImage = function (pixelAry) {
+      this.pixels = _.map(pixelAry, function (p) {
+        return _.pick(p, ["x", "y", "color"]);
+      });
+      this.paint();
+    };
 
-        _.each(pixels, function(p) {
-          if(p.x >= 0 && p.x < pixelWidth && p.y >= 0 && p.y < pixelHeight){
-            pCanvas.setPixel(p.x, p.y, p.color);
-          }
-        });
 
-        pCanvas.paint();
+    // paint writes all stored pixels to the PixelCanvas and calls the
+    // PixelCanvas" paint method
+    PixelColorer.prototype.paint = function () {
+      var context = this.$htmlCanvas[0].getContext("2d");
+      var i = 0;
+      var sparams = this.pCanvas.screenParams(this.pixelWidth,
+                                              this.pixelHeight);
 
-        if(!showGrid) return;
-
-        // draw grid system after pixels have been painted, for visibility
-        context.beginPath();
-
-        for( ; i<=pixelWidth; i++){
-          context.moveTo(sparams.xoffset + i*sparams.pixelSize,
-                         sparams.yoffset);
-          context.lineTo(sparams.xoffset + i*sparams.pixelSize,
-                         sparams.yoffset + pixelHeight*sparams.pixelSize);
+      _.each(this.pixels, function(p) {
+        if(p.x >= 0 && p.x < this.pixelWidth && p.y >= 0 &&
+           p.y < this.pixelHeight){
+          this.pCanvas.setPixel(p.x, p.y, p.color);
         }
+      }, this);
 
-        for(i=0 ; i<=pixelHeight; i++){
-          context.moveTo(sparams.xoffset,
-                         sparams.yoffset + i*sparams.pixelSize);
-          context.lineTo(sparams.xoffset + pixelWidth*sparams.pixelSize,
-                         sparams.yoffset + i*sparams.pixelSize);
-        }
+      this.pCanvas.paint();
 
-        context.closePath();
-        context.strokeStyle = "#777777";
-        context.stroke();
-      };
+      if(!this.showGrid) return;
 
+      // draw grid system after pixels have been painted, for visibility
+      context.beginPath();
 
-      // click registers onclick callback for canvas to run after the body
-      // PixelCanvas onclick event has run
-      //
-      // Arguments:
-      //   callbackFunction: A function that may optionally take a jQuery click
-      //                     event to do further processing with the click
-      this.mousemove = function (callbackFunction) {
-        mouseMoveAction = callbackFunction;
-      };
+      for( ; i<=this.pixelWidth; i++){
+        context.moveTo(sparams.xoffset + i*sparams.pixelSize,
+                       sparams.yoffset);
+        context.lineTo(sparams.xoffset + i*sparams.pixelSize,
+                       sparams.yoffset + this.pixelHeight*sparams.pixelSize);
+      }
 
+      for(i=0 ; i<=this.pixelHeight; i++){
+        context.moveTo(sparams.xoffset,
+                       sparams.yoffset + i*sparams.pixelSize);
+        context.lineTo(sparams.xoffset + this.pixelWidth*sparams.pixelSize,
+                       sparams.yoffset + i*sparams.pixelSize);
+      }
 
-      // resize resizes the number of meta-pixels available for drawing
-      // on the canvas element
-      //
-      // Arguments:
-      //   width: width of the pixel canvas in meta-pixels
-      //   height: height of the pixel canvas in meta-pixels
-      this.resize = function (width, height){
-        pixelWidth = width;
-        pixelHeight = height;
-        pCanvas = new PixelCanvas(pixelWidth, pixelHeight, backgroundColor,
-                                  canvasID);
-        this.paint();
-      };
+      context.closePath();
+      context.strokeStyle = "#777777";
+      context.stroke();
+    };
 
 
-      // setAction sets the action that will be performed when a pixel is
-      // clicked on
-      //
-      // Arguments:
-      //   actionString: One of the following strings -
-      //                 "clear", returns the pixel to the default color of the 
-      //                          canvas
-      //                 "get", returns the color of the pixel clicked on
-      //                 "set", sets the color of the pixel clicked on
-      this.setAction = function (actionString) {
-        action = actionString;
-      };
+    // click registers onclick callback for canvas to run after the body
+    // PixelCanvas onclick event has run
+    //
+    // Arguments:
+    //   callbackFunction: A function that may optionally take a jQuery click
+    //                     event to do further processing with the click
+    PixelColorer.prototype.mousemove = function (callbackFunction) {
+      this.mouseMoveAction = callbackFunction;
+    };
 
 
-      // setBackgroundColor sets the background color of the pixel canvas,
-      // where the default is #FFFFFF
-      //
-      // Arguments:
-      //   color: a hexadecimal string "#RRGGBB"
-      this.setBackgroundColor = function (color) {
-        backgroundColor = Color.sanitize(color);
-        pCanvas = new PixelCanvas(pixelWidth, pixelHeight, backgroundColor,
-                                  canvasID);
-        this.paint();
-      };
+    // resize resizes the number of meta-pixels available for drawing
+    // on the canvas element
+    //
+    // Arguments:
+    //   width: width of the pixel canvas in meta-pixels
+    //   height: height of the pixel canvas in meta-pixels
+    PixelColorer.prototype.resize = function (width, height){
+      this.pixelWidth = width;
+      this.pixelHeight = height;
+      this.pCanvas = new PixelCanvas(this.pixelWidth, this.pixelHeight,
+                                     this.backgroundColor, this.canvasID);
+      this.paint();
+    };
 
 
-      // setColor sets the current color that will be drawn on pixels that are
-      // clicked on
-      //
-      // Arguments:
-      //   color: a hexadecimal string in the format "#RRGGBB"
-      this.setColor = function (color) {
-        currentColor = Color.sanitize(color);
-      };
+    // setAction sets the action that will be performed when a pixel is
+    // clicked on
+    //
+    // Arguments:
+    //   actionString: One of the following strings -
+    //                 "clear", returns the pixel to the default color of the 
+    //                          canvas
+    //                 "get", returns the color of the pixel clicked on
+    //                 "set", sets the color of the pixel clicked on
+    PixelColorer.prototype.setAction = function (actionString) {
+      this.action = actionString;
+    };
 
 
-      // toggleGrid toggles whether to display the grid of pixel boundrys or not
-      this.toggleGrid = function () {
-        showGrid = !showGrid;
-        this.paint();
-      };
+    // setBackgroundColor sets the background color of the pixel canvas,
+    // where the default is #FFFFFF
+    //
+    // Arguments:
+    //   color: a hexadecimal string "#RRGGBB"
+    PixelColorer.prototype.setBackgroundColor = function (color) {
+      this.backgroundColor = Color.sanitize(color);
+      this.pCanvas = new PixelCanvas(this.pixelWidth, this.pixelHeight,
+                                     this.backgroundColor, this.canvasID);
+      this.paint();
+    };
+
+
+    // setColor sets the current color that will be drawn on pixels that are
+    // clicked on
+    //
+    // Arguments:
+    //   color: a hexadecimal string in the format "#RRGGBB"
+    PixelColorer.prototype.setColor = function (color) {
+      this.currentColor = Color.sanitize(color);
+    };
+
+
+    // toggleGrid toggles whether to display the grid of pixel boundrys or not
+    PixelColorer.prototype.toggleGrid = function () {
+      this.showGrid = !this.showGrid;
+      this.paint();
     };
 
     return PixelColorer;
