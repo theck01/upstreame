be903fb116cd89f92be59894846ead4690edf9f0
Tyler Heck <tyler.heck@gmail.com>
2013-10-22 21:31:56 -0500

    rewrite keypoll to function at all

--- public/scripts/interface/keypoll.js
+++ public/scripts/interface/keypoll.js
@@ -3,39 +3,25 @@ define(['jquery'], function ($) {
   // KeyPoll object can be bound to a dom element, and listens for key events
   // related to that element. States of any key can be queried, a key is
   // currently pressed if polling returns true, if not polling returns false.
-  var KeyPoll = {
-    keys: Object.create(null)
-  };
-
-
-  // Bind an instance of KeyPoll to the domID specified.
   //
   // Arguments:
   //   domID: CSS style selector used to specify which element to bind the
   //          key listening events for this instance of KeyPoll.
   //          Default: window
-  KeyPoll.prototype.bind = function (domID) {
+  var KeyPoll = function (domID) {
     var keypoll = this;
+
+    keypoll.keys = Object.create(null);
 
     if (!domID) domID = window;
 
     $(domID).keyup(function (e) {
-      keypoll.clear(e.which);
+      keypoll.keys[e.which] = false;
     });
 
     $(domID).keydown(function (e) {
-      keypoll.set(e.which);
+      keypoll.keys[e.which] = true;
     });
-  };
-
-
-  // Clear key with keycode from storage, indicating that it is no longer
-  // pressed
-  //
-  // Arguments:
-  //   keycode: Code identifiying the key to clear
-  KeyPoll.prototype.clear = function (keycode) {
-    this.keys[keycode] = false;
   };
 
 
@@ -48,15 +34,6 @@ define(['jquery'], function ($) {
   //   Boolean, true if key is pressed, false if not
   KeyPoll.prototype.poll = function (keycode) {
     return !!this.keys[keycode];
-  };
-
-
-  // Set key with keycode in storage, indicating that it is currently pressed
-  //
-  // Arguments:
-  //   keycode: Code identifiying the key to set
-  KeyPoll.prototype.set = function (keycode) {
-    this.keys[keycode] = true;
   };
 
   
