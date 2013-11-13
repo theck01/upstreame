2725b05a98e61461a7c8df3cbebe7867957866d5
Tyler Heck <tyler.heck@gmail.com>
2013-11-12 19:33:32 -0600

    document and test priorityqueue.js

--- public/scripts/util/priorityqueue.js
+++ public/scripts/util/priorityqueue.js
@@ -8,13 +8,19 @@ define([], function () {
     var cindicies = childrenIndicies(index);
     var priorityIndex;
 
-    if (comp(heap[cindicies[0]], heap[cindicies[1]])) {
+    // if the children indicies are outside of the heap bounds downHeap
+    // operation is complete
+    if (cindicies[0] >= heap.length) return;
+
+    // if only one child is in heap bounds, downHeap that one child
+    if (cindicies[1] >= heap.length) priorityIndex = cindicies[0];
+    // else compare with the highest priority child
+    else if (comp(heap[cindicies[0]], heap[cindicies[1]])) {
       priorityIndex = cindicies[0];
     }
     else priorityIndex = cindicies[1];
 
-    if (!comp(heap[index], heap[cindicies[0]]) ||
-        !comp(heap[index], heap[cindicies[1]])) {
+    if (comp(heap[priorityIndex], heap[index])) {
       var temp = heap[index];
       heap[index] = heap[priorityIndex];
       heap[priorityIndex] = temp;
@@ -27,7 +33,12 @@ define([], function () {
   }
 
   function upHeap (heap, index, comp) {
+    // if index is the top of the heap or beyond bounds, upHeap operation is
+    // complete
+    if (index <= 0) return;
+
     var pindex = parentIndex(index);
+
     if (!comp(heap[pindex], heap[index])) {
       var temp = heap[pindex];
       heap[pindex] = heap[index];
@@ -36,27 +47,44 @@ define([], function () {
     }
   }
 
+  // PriorityQueue, queue ordered by comparision function of elements, or <=
+  //
+  // Arguments:
+  //   compFunction: Comparision function. If first argument is of higher
+  //                 priority return true, else return false
   var PriorityQueue = function (compFunction) {
     this.comp = compFunction || function (a, b) { return a <= b; };
     this.heap = [];
   };
 
 
+  // peek returns highest priority element without removing it from the queue
+  //
+  // Returns:
+  //   Highest priority element in the queue
   PriorityQueue.prototype.peek = function () {
     return this.heap[0];
   };
 
 
+  // pop returns highest priority element and removes it from the queue
+  //
+  // Returns:
+  //   Highest priority element in the queue
   PriorityQueue.prototype.pop = function () {
     var retval = this.heap[0];
 
-    this.heap[0] = this.heap.pop();
-    downHeap(this.heap, 0, this.comp);
+    var lastElement = this.heap.pop();
+    if (this.heap.length > 0) {
+      this.heap[0] = lastElement;
+      downHeap(this.heap, 0, this.comp);
+    }
 
     return retval;
   };
 
 
+  // push adds an element to the appropriate location in the priority queue
   PriorityQueue.prototype.push = function (elem) {
     this.heap.push(elem);
     upHeap(this.heap, this.heap.length - 1, this.comp);
---
+++ spec/public/priorityqueue.spec.js
@@ -0,0 +1,87 @@
+var assert = require('assert');
+var requirejs = require('requirejs');
+var should = require('should');
+var _ = require('underscore');
+
+requirejs.config({
+  baseUrl: 'public/scripts',
+  nodeRequire: require,
+  paths: {
+    underscore: 'bower_components/underscore-amd/underscore-min'
+  }
+});
+
+var PriorityQueue = requirejs('util/priorityqueue');
+
+describe('PriorityQueue', function () {
+  var pq;
+
+  beforeEach(function (done) {
+    pq = new PriorityQueue(function (a, b) {
+      if (a > b) {
+        return true;
+      }
+      return false;
+    });
+
+    _.each([9,4,6,1,5,2], function (e) {
+      pq.push(e);
+    });
+
+    done();
+  });
+
+  describe('PriorityQueue#peek', function () {
+    context('when queue is not empty', function () {
+      it('should return the highest priority element without removal',
+        function (done) {
+          pq.peek().should.eql(9);
+          pq.peek().should.eql(9);
+          done();
+        }
+      );
+    });
+
+    context('when queue is empty', function () {
+      it('should return undefined', function (done) {
+        for(var i=0; i<6; i++) pq.pop();
+        assert.equal(pq.peek(), undefined);
+        done();
+      });
+    });
+  });
+
+  describe('PriorityQueue#pop', function () {
+    context('when queue is not empty', function () {
+      it('should return elements in order of highest priority with removal',
+        function (done) {
+          _.each([9,6,5,4,2,1], function (e) {
+            pq.pop().should.eql(e);
+          });
+          done();
+        }
+      );
+    });
+
+
+    context('when queue is empty', function () {
+      it('should return undefined', function (done) {
+        for(var i=0; i<6; i++) pq.pop();
+        assert.equal(pq.pop(), undefined);
+        done();
+      });
+    });
+  });
+
+  describe('PriorityQueue#push', function () {
+    it('should add an element to the appropriate place in the queue',
+      function (done) {
+        pq.push(10);
+        pq.peek().should.eql(10);
+        pq.push(3);
+        pq.peek().should.eql(10);
+        done();
+      }
+    );
+  });
+});
