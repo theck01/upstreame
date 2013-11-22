f1f3d30e08af3b6d38c54b1149aa90eae036de75
Tyler Heck <tyler.heck@gmail.com>
2013-11-21 20:29:20 -0600

    add shade method to Color

--- public/scripts/graphics/color.js
+++ public/scripts/graphics/color.js
@@ -4,7 +4,7 @@ define([], function () {
   // (objects with red, green, and blue fields)
   var Color = {};
 
-  // sanitize takes a color hexadecimal string and returns a properly
+  // sanitize takes a color hexadecimal string and returns a properly formatted
   // color hexadecimal string
   //
   // Arguments:
@@ -29,6 +29,44 @@ define([], function () {
     }
 
     return "#FFFFFF";
+  };
+
+
+  // shade a single component of a hexadecimal color
+  //
+  // Arguments:
+  //   color: A two digit hexadecimal string
+  //   change: An integer value to offset each RGB field by. Negative changes
+  //           darken colors, positive changes brighten them
+  //
+  // Returns: A two digit hexadecimal string
+  function shadeComponent (comp, change) {
+    comp = parseInt(comp, 16) + change;
+    comp = Math.max(Math.min(comp, 255), 0).toString(16);
+    return ("0" + comp).slice(-2);
+  }
+  
+  // shade brightens or darkens a hexadecimal color string
+  //
+  // Arguments:
+  //   color: A hexidecimal color string in the form "#RRGGBB"
+  //   change: An integer value to offset each RGB field by. Negative changes
+  //           darken colors, positive changes brighten them
+  //
+  // Returns: A hexidecimal color string in the form "#RRGGBB"
+  Color.shade = function (color, change) {
+    color = Color.sanitize(color);
+
+    var red = color.substr(1,2);
+    var green = color.substr(3,2);
+    var blue = color.substr(5,2);
+
+    var shadedColor = "#";
+    shadedColor += shadeComponent(red, change);
+    shadedColor += shadeComponent(green, change);
+    shadedColor += shadeComponent(blue, change);
+
+    return shadedColor;
   };
 
   return Color;
