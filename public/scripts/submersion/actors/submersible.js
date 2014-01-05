780add654c9b656505ec1f652ab98530ced3e6bd
Tyler Heck <tyler.heck@gmail.com>
2014-01-05 10:26:46 -0600

    submersible actor uses all rotation variant sprites during movement

--- public/scripts/submersion/actors/submersible.js
+++ public/scripts/submersion/actors/submersible.js
@@ -13,6 +13,9 @@ define(['underscore', 'core/graphics/spritearchive', 'core/actors/base'],
       var PITCH_STEP = 20;
       var PITCH_MIN = -20;
       var PITCH_MAX = 20;
+      var DIAGONAL_PITCH_STEP = 10;
+      var DIAGONAL_PITCH_MIN = -10;
+      var DIAGONAL_PITCH_MAX = 10;
       
       // Submarine actor, controlled directly by keyboard input
       //
@@ -32,7 +35,7 @@ define(['underscore', 'core/graphics/spritearchive', 'core/actors/base'],
         this.keypoll = opts.keypoll;
         this.pitch = 0;
         this.yaw = 170;
-        this.direction = { x: 1, y: 0 };
+        this.direction = { x: 1, xvel: 0, y: 0 };
 
         var sub = this;
         this.scheduledRotation = this.frameClock.recurring(function () {
@@ -45,9 +48,18 @@ define(['underscore', 'core/graphics/spritearchive', 'core/actors/base'],
 
           if (sub.direction.y === 0) sub.pitch = 0;
           else {
-            sub.pitch = Math.min(sub.pitch + PITCH_STEP * sub.direction.y * -1,
-                                 PITCH_MAX);
-            sub.pitch = Math.max(sub.pitch, PITCH_MIN);
+            // check for special case of full diagonal movement
+            if ((sub.yaw === YAW_MIN && sub.direction.xvel === -1) ||
+                (sub.yaw === YAW_MAX && sub.direction.xvel === 1)) {
+              sub.pitch += DIAGONAL_PITCH_STEP * sub.direction.y * -1;
+              sub.pitch = Math.min(sub.pitch, DIAGONAL_PITCH_MAX);
+              sub.pitch = Math.max(sub.pitch, DIAGONAL_PITCH_MIN);
+            }
+            else {
+              sub.pitch += PITCH_STEP * sub.direction.y * -1;
+              sub.pitch = Math.min(sub.pitch, PITCH_MAX);
+              sub.pitch = Math.max(sub.pitch, PITCH_MIN);
+            }
           }
 
           if (sub.pitch < 0) spriteName += 'neg' + (-1 * sub.pitch).toString();
@@ -74,6 +86,7 @@ define(['underscore', 'core/graphics/spritearchive', 'core/actors/base'],
 
         // set direction according to most recent key press
         if (horizontalChange !== 0) this.direction.x = horizontalChange;
+        this.direction.xvel = horizontalChange;
         this.direction.y = verticalChange;
 
         // if not fully rotated for direction of movement, move at reduced speed
@@ -84,8 +97,10 @@ define(['underscore', 'core/graphics/spritearchive', 'core/actors/base'],
            speed.x = OPPOSING_SPEED;
            speed.diagx = OPPOSING_DIAGONAL_SPEED;
         }
-        if ((this.direction.y === 1 && this.pitch !== PITCH_MIN) ||
-            (this.direction.y === -1 && this.pitch !== PITCH_MAX)) {
+        if ((this.direction.y === 1 && this.pitch !== PITCH_MIN &&
+             this.pitch !== DIAGONAL_PITCH_MIN) ||
+            (this.direction.y === -1 && this.pitch !== PITCH_MAX &&
+             this.pitch !== DIAGONAL_PITCH_MAX)) {
            speed.y = OPPOSING_SPEED;
            speed.diagy = OPPOSING_DIAGONAL_SPEED;
         }
