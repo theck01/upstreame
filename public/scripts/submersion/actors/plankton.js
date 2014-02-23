538c58f315fb1a4547ef2513e5c2ad81a1a8e0c4
Tyler Heck <tyler.heck@gmail.com>
2014-02-23 09:55:18 -0500

    finish initial Plankton actor

--- public/scripts/submersion/actors/plankton.js
+++ public/scripts/submersion/actors/plankton.js
@@ -2,12 +2,6 @@ define(['underscore', 'core/actors/base', 'core/graphics/sprite',
         'submersion/util/group', 'submersion/util/layer'],
   function (_, Base, Sprite, Group, Layer) {
 
-    var BASE_PLANKTON_LAYER = Layer.nearBackground;
-    var TOP_PLANKTON_LAYER = Layer.frontFocus;
-    var PLANKTON_COLOR = '#DDD';
-    var DRIFT_FREQUENCY = 5;
-
-
     // randomPixels returns an array of between 1-4 pixels
     function randomPixels (count) {
       var seedPixels = [
@@ -23,7 +17,7 @@ define(['underscore', 'core/actors/base', 'core/graphics/sprite',
         returnPixels.push(seedPixels.splice(i, 1)[0]);
       }
 
-      return randomPixels;
+      return returnPixels;
     }
       
     
@@ -33,33 +27,40 @@ define(['underscore', 'core/actors/base', 'core/graphics/sprite',
     //   center: Center of the object, essentially location in the world
     //   layer: Layer that it occupies in a LayeredCanvas heirarchy
     var Plankton = function (opts) {
-      if (opts.layer < BASE_PLANKTON_LAYER ||
-          opts.layer > TOP_PLANKTON_LAYER) {
+      if (opts.layer < Plankton.BASE_PLANKTON_LAYER ||
+          opts.layer > Plankton.TOP_PLANKTON_LAYER) {
         throw new Error('Plankton cannot be created on layer ' + opts.layer);
       }
 
-      var size = opts.layer - BASE_PLANKTON_LAYER;
+      var size = opts.layer - Plankton.BASE_PLANKTON_LAYER;
       opts.sprite = new Sprite(randomPixels(size));
       opts.group = 'Plankton';
-      opts.noncollidables = Group.collect('animals');
+      opts.noncollidables = Group.collect('friendlies');
 
       Base.call(this, opts);
-
-      // Plankton cannot collide with anything
-      this.unregister('actionbox.collisions');
 
       this.steps = 0;
     };
     Plankton.prototype = Object.create(Base.prototype);
     Plankton.prototype.constructor = Plankton;
 
+    Plankton.BASE_PLANKTON_LAYER = Layer.nearBackground;
+    Plankton.TOP_PLANKTON_LAYER = Layer.frontFocus;
+    Plankton.DRIFT_FREQUENCY = 5;
+    var HORIZONTAL_DRIFT_FREQUENCY = Plankton.DRIFT_FREQUENCY * 5;
+    var PLANKTON_COLOR = '#DDD';
+
     
-    // overloaded Base#act function
-    Plankton.prototype.act = function () {
+    // overloaded Base#_act function
+    Plankton.prototype._act = function () {
       if (this.steps === 0) {
         this.move({ x: Math.floor(Math.random()*3 - 1), y: 1 });
       }
-      this.steps = (this.steps + 1)%DRIFT_FREQUENCY;
+      else if (this.steps%Plankton.DRIFT_FREQUENCY === 0) {
+        this.move({ x: 0, y: 1 });
+      }
+
+      this.steps = (this.steps + 1)%HORIZONTAL_DRIFT_FREQUENCY;
     };
 
 
