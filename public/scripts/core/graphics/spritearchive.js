4d6079d67fe369df5ec2dfba24efc6ba815fa825
Tyler Heck <tyler.heck@gmail.com>
2014-03-02 18:23:54 -0500

    SpriteArchive uses new model format

--- public/scripts/core/graphics/spritearchive.js
+++ public/scripts/core/graphics/spritearchive.js
@@ -32,7 +32,8 @@ define(['underscore', 'core/graphics/sprite'], function (_, Sprite) {
   //               containing raw sprite data and the center of the raw sprites
   SpriteArchive.load = function (rawSprites) {
     _.each(rawSprites, function (v, k) {
-      this.add(k, v.pixels);
+      if (v.pixels) this.add(k, v.pixels);
+      else this.add(k, v.elements);
     }, this);
   };
 
