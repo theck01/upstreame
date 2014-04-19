b9194f981c38eb1dbe17c1e3891900e537baeb6a
Tyler Heck <tyler.heck@gmail.com>
2014-04-19 00:29:15 -0400

    add GridModel

---
+++ public/scripts/core/model/gridmodel.js
@@ -0,0 +1,180 @@
+define(['underscore', 'core/util/encoder'], function (_, Encoder) {
+
+  // GridModel is the base model used to create Sprites, Worlds, and other
+  // grid based entities
+  //
+  // Arguments:
+  //   opt_dimensions: Optional object with 'width' and 'height' fields, the
+  //                   dimensions of the full model. Will be expanded as needed.
+  var GridModel = function (opt_dimensions) {
+    this._dim = _.clone(opt_dimensions) || { width: 1, height: 1 };
+    this._elements = [];
+    this._offset = { x: 0, y: 0 };
+  };
+
+
+  // actions available when applying changes to a GridModel
+  GridModel.MODEL_ACTIONS = {
+    CLEAR: 'clear',
+    SET: 'set'
+  };
+
+
+  // applyChanges applies an array of changes to the model, mutating its
+  // internal state.
+  //
+  // Arguments:
+  //   changes: Array of objects with 'action' and 'elements' fields.
+  //     action: An string value in the GridModel.MODEL_ACTION object.
+  //     elements: An object with at least 'x', 'y' and 'color' fields.
+  GridModel.prototype.applyChanges = function (changes) {
+    this._elements = this._elementsWithChanges(changes);
+  };
+
+
+  // clear drops all elements within the model.
+  GridModel.prototype.clear = function () {
+    this._elements = [];
+  };
+
+
+  // _createElementMap generates a map from encoded element coordinates to
+  // elements.
+  //
+  // Arguments:
+  //   opt_elements: Optional array of objects with at least 'x', 'y' and
+  //                 'color' fields.
+  GridModel.prototype._createElementMap = function (opt_elements) {
+    return _.reduce(opt_elements || this._elements, function (map, e) {
+      map[Encoder.coordToScalar(this._offsetElement(e), this._dim)] = e;
+      return map;
+    }, Object.create(null), this);
+  };
+
+
+  // elementsWithChanges generates the array of elements created when applying
+  // the changes to the internal model elements.
+  //
+  // Arguments:
+  //   changes: Array of objects with 'action' and 'elements' fields.
+  //     action: An string value in the GridModel.MODEL_ACTION object.
+  //     elements: An object with at least 'x', 'y' and 'color' fields.
+  // Returns an array of objects with at least 'x', 'y', and 'color' fields.
+  GridModel.prototype._elementsWithChanges = function (changes) {
+    var existingElementMap = this._createElementMap();
+
+    _.each(changes, function (change) {
+      _.each(change.elements, function (e) {
+        var offsetCoord = this._offsetElement(e);
+
+        // If the element will not fit into the model dimensions, update
+        // dimensions to allow element to be inserted.
+        if (offsetCoord.x >= this._dim.width || offsetCoord.x < 0 ||
+            offsetCoord.y >= this._dim.height || offsetCoord.y < 0) {
+          this._updateSizing(offsetCoord);
+          existingElementMap = this._createElementMap(
+              _.values(existingElementMap));
+          offsetCoord = this._offsetElement(e);
+        }
+
+        var encoded = Encoder.coordToScalar(offsetCoord, this._dim);
+
+        if (change.action === GridModel.MODEL_ACTIONS.CLEAR &&
+            _.has(existingElementMap, encoded)) {
+          delete existingElementMap[encoded];
+        }
+        else if (change.action === GridModel.MODEL_ACTIONS.SET) {
+          existingElementMap[encoded] = e;
+        }
+      }, this);
+    }, this);
+
+    return _.values(existingElementMap);
+  };
+
+
+  // getElements within the window given by the Frame instance, in coordinates
+  // relative to the frame instance.
+  //
+  // Arguments:
+  //   frame: The frame within which returns elements must lie.
+  //   opt_changes: Optional set of changes to apply to the elements before
+  //                retrieval.
+  // Returns an array of objects with at least 'x', 'y', and 'color' fields.
+  GridModel.prototype.getElements = function (frame, opt_changes) {
+    var elements = opt_changes ?
+      this._elementsWithChanges(opt_changes) : this._elements;
+
+    return _.reduce(elements, function (memo, e) {
+      if (frame.containsCoord(e)) {
+        memo.push(_.extend(_.clone(e), frame.relativePosition(e)));
+      }
+      return memo;
+    }, []);
+  };
+
+
+  // getPosition returns the current offset and dimensions maintained by the
+  // GridModel instance
+  //
+  // Returns an object with 'offset' and  'dimensions' fields
+  GridModel.prototype.getPosition = function () {
+    return {
+      offset: _.clone(this._offset),
+      dimensions: _.clone(this._dim)
+    };
+  };
+
+
+  // _offsetElement returns the coordinate location of the element offset by
+  // the internal model offset.
+  //
+  // Arguments:
+  //   element: object with at least 'x', 'y', and 'color' fields.
+  // Returns an object with 'x' and 'y' fields.
+  GridModel.prototype._offsetElement = function (element) {
+    return { x: element.x + this._offset.x, y: element.y + this._offset.y };
+  };
+
+
+  // updateCoverage ensures that the model covers at least the area given by the
+  // offset and dimensions, or more if elements already exist beyond the resize
+  // area.
+  //
+  // Arguments:
+  //    dimensions: Object with 'width' and 'height' fields
+  //    offset: Object with positive integer 'x' and 'y' fields
+  GridModel.prototype.updateCoverage = function (dimensions, offset) {
+    this._offset.x = Math.max(this._offset.x, offset.x);
+    this._offset.y = Math.max(this._offset.y, offset.y);
+    this._dim.width = Math.max(this._dim.width, dimensions.width);
+    this._dim.height = Math.max(this._dim.height, dimensions.height);
+  };
+
+
+  // _updateSizing updates the dimensions and offset of the model object to
+  // fit the new coordinate.
+  //
+  // Arguments:
+  //    offsetCoord: The offset coordinate tha does not fit within the existing
+  //                 model dimensions.
+  GridModel.prototype._updateSizing = function (offsetCoord) {
+    if (offsetCoord.x < 0) {
+      this._offset.x += -offsetCoord.x;
+      this._dim.width += -offsetCoord.x;
+    }
+    else if (this._dim.width <= offsetCoord.x) {
+      this._dim.width = offsetCoord.x + 1;
+    }
+    if (offsetCoord.y < 0) {
+      this._offset.y += -offsetCoord.y;
+      this._dim.height += -offsetCoord.y;
+    }
+    else if (this._dim.height <= offsetCoord.y) {
+      this._dim.height = offsetCoord.y + 1;
+    }
+  };
+
+
+  return GridModel;
+});
---
+++ spec/public/core/model/gridmodel.spec.js
@@ -0,0 +1,292 @@
+var assert = require('assert');
+var requirejs = require('requirejs');
+var _ = require('underscore');
+
+requirejs.config({
+  baseUrl: 'public/scripts',
+  nodeRequire: require,
+  paths: {
+    underscore: 'bower_components/underscore-amd/underscore-min'
+  }
+});
+
+var GridModel = requirejs('core/model/gridmodel');
+var Encoder = requirejs('core/util/encoder');
+var Frame = requirejs('core/util/frame');
+
+
+function makeEdits(gridModel) {
+  gridModel.applyChanges([
+    {
+      action: GridModel.MODEL_ACTIONS.SET,
+      elements: [
+        { x: 0, y: 0, color: '#FFFFFF' },
+        { x: 1, y: 0, color: '#FFFFFF' },
+        { x: 0, y: 1, color: '#FFFFFF' },
+        { x: 1, y: 1, color: '#FFFFFF' }
+      ]
+    },
+    {
+      action: GridModel.MODEL_ACTIONS.CLEAR,
+      elements: [
+        { x: 1, y: 0, color: '#FFFFFF' },
+        { x: 1, y: 1, color: '#FFFFFF' }
+      ]
+    },
+    {
+      action: GridModel.MODEL_ACTIONS.SET,
+      elements: [
+        { x: 1, y: 0, color: '#000000' },
+      ]
+    }
+  ]);
+
+  return [
+    { x: 0, y: 0, color: '#FFFFFF' },
+    { x: 0, y: 1, color: '#FFFFFF' },
+    { x: 1, y: 0, color: '#000000' }
+  ];
+}
+
+
+function assertModelHasElements(model, frame, elements, opt_changes) {
+  var oa1 = model.getElements(frame, opt_changes ? opt_changes : []);
+  var oa2 = elements;
+
+  var modelPosition = model.getPosition();
+  var dimensions = {};
+  dimensions.width = modelPosition.dimensions.width + modelPosition.offset.x;
+  dimensions.height = modelPosition.dimensions.height + modelPosition.offset.y;
+
+  if (oa1.length !== oa2.length) {
+    throw new Error('Element arrays not equivalent lengths');
+  }
+
+  oa1 = _.sortBy(oa1, function (p) {
+    return Encoder.coordToScalar(model._offsetElement(p), dimensions);
+  });
+  oa2 = _.sortBy(oa2, function (p) {
+    return Encoder.coordToScalar(model._offsetElement(p), dimensions);
+  });
+
+  var zipped = _.zip(oa1, oa2);
+
+  _.each(zipped, function (pair) {
+    if (!_.isEqual(pair[0], pair[1])) {
+      throw new Error('Model does not have expected elements.');
+    }
+  });
+}
+
+
+describe('GridModel', function () {
+  var gridModel;
+  var frame;
+
+
+  beforeEach(function () {
+    gridModel = new GridModel();
+    frame = new Frame({ width: 5, height: 5 }, { x: 0, y: 0 });
+  });
+
+
+  describe('applyChanges', function () {
+    context('when applying "' + GridModel.MODEL_ACTIONS.SET + '" changes',
+      function () {
+        it('should set new elements to specified positions', function () {
+          frame.resize({ width: 102, height: 1001 });
+          frame.move({ x: -1, y: 0 });
+
+          var setElements = [
+            { x: 0, y: 0, color: '#FFFFFF' },
+            { x: 100, y: 1000, color: '#FFFFFF' },
+            { x: -1, y: 0, color: '#FFFFFF' }
+          ];
+
+          gridModel.applyChanges([{
+            action: GridModel.MODEL_ACTIONS.SET,
+            elements: setElements
+          }]);
+
+          var relativeElements = [
+            { x: 1, y: 0, color: '#FFFFFF' },
+            { x: 101, y: 1000, color: '#FFFFFF' },
+            { x: 0, y: 0, color: '#FFFFFF' }
+          ];
+          var elements = gridModel.getElements(frame);
+
+          assert(elements.length === 3);
+          assertModelHasElements(gridModel, frame, relativeElements);
+        });
+
+        it('should overwrite existing elements', function () {
+          frame.resize({ width: 102, height: 1001 });
+          frame.move({ x: -1, y: 0 });
+
+          var setElements = [
+            { x: 0, y: 0, color: '#FFFFFF' },
+            { x: 100, y: 1000, color: '#FFFFFF' },
+            { x: -1, y: 0, color: '#FFFFFF' }
+          ];
+
+          var overrideElements = [
+            { x: 0, y: 0, color: '#000000' },
+            { x: 100, y: 1000, color: '#000000' },
+          ];
+
+          gridModel.applyChanges([
+            {
+              action: GridModel.MODEL_ACTIONS.SET,
+              elements: setElements
+            },
+            {
+              action: GridModel.MODEL_ACTIONS.SET,
+              elements: overrideElements
+            }
+          ]);
+
+          var relativeElements = [
+            { x: 1, y: 0, color: '#000000' },
+            { x: 101, y: 1000, color: '#000000' },
+            { x: 0, y: 0, color: '#FFFFFF' }
+          ];
+          var elements = gridModel.getElements(frame);
+
+          assert(elements.length === 3);
+          assertModelHasElements(gridModel, frame, relativeElements);
+        });
+      }
+    );
+
+    context('when applying "' + GridModel.MODEL_ACTIONS.CLEAR + '"changes',
+      function () {
+        it('should clear elements if they exist in the model', function () {
+          frame.resize({ width: 102, height: 1001 });
+          frame.move({ x: -1, y: 0 });
+
+          var setElements = [
+            { x: 0, y: 0, color: '#FFFFFF' },
+            { x: 100, y: 1000, color: '#FFFFFF' },
+            { x: -1, y: 0, color: '#FFFFFF' }
+          ];
+
+          var clearElements = [
+            { x: 100, y: 1000, color: '#FFFFFF' },
+            { x: -1, y: 0, color: '#FFFFFF' }
+          ];
+
+          gridModel.applyChanges([
+            {
+              action: GridModel.MODEL_ACTIONS.SET,
+              elements: setElements
+            },
+            {
+              action: GridModel.MODEL_ACTIONS.CLEAR,
+              elements: clearElements
+            }
+          ]);
+
+          var relativeElements = [{ x: 1, y: 0, color: '#FFFFFF' }];
+          var elements = gridModel.getElements(frame);
+
+          assert(elements.length === 1);
+          assertModelHasElements(gridModel, frame, relativeElements);
+        });
+
+
+        it('should do nothing if cleared elements don\'t exist', function () {
+          frame.resize({ width: 102, height: 1001 });
+          frame.move({ x: -1, y: 0 });
+
+          var setElements = [
+            { x: 0, y: 0, color: '#FFFFFF' },
+            { x: 100, y: 1000, color: '#FFFFFF' },
+            { x: -1, y: 0, color: '#FFFFFF' }
+          ];
+
+          var clearElements = [
+            { x: 101, y: 1000, color: '#FFFFFF' },
+            { x: -1, y: -1, color: '#FFFFFF' }
+          ];
+
+          gridModel.applyChanges([
+            {
+              action: GridModel.MODEL_ACTIONS.SET,
+              elements: setElements
+            },
+            {
+              action: GridModel.MODEL_ACTIONS.CLEAR,
+              elements: clearElements
+            }
+          ]);
+
+          var relativeElements = [
+            { x: 1, y: 0, color: '#FFFFFF' },
+            { x: 101, y: 1000, color: '#FFFFFF' },
+            { x: 0, y: 0, color: '#FFFFFF' }
+          ];
+          var elements = gridModel.getElements(frame);
+
+          assert(elements.length === 3);
+          assertModelHasElements(gridModel, frame, relativeElements);
+        });
+      }
+    );
+  });
+
+
+  describe('clear', function () {
+    it('should remove all elements from the GridModel', function () {
+      makeEdits(gridModel);
+      gridModel.clear();
+      assert(gridModel.getElements(frame).length === 0);
+    });
+  });
+
+
+  describe('getElements', function () {
+    context('when no additional changes supplied', function () {
+      it('should return elements within the frame relative to the frame',
+        function () {
+          var expectedElements = makeEdits(gridModel);
+
+          assertModelHasElements(gridModel, frame, expectedElements);
+
+          frame.resize({ width: 2, height: 1 });
+          frame.move({ x: 0, y: 1 });
+
+          expectedElements = [{ x: 0, y: 0, color: '#FFFFFF' }];
+
+          assertModelHasElements(gridModel, frame, expectedElements);
+        }
+      );
+    });
+
+    context('when additional changes are supplied', function () {
+      it('should return elements from the model and the changes', function () {
+        var expectedElements = makeEdits(gridModel);
+        var changeElement = { x: 3, y: 3, color: '#7777777' };
+        var changes = [{
+          action: GridModel.MODEL_ACTIONS.SET,
+          elements: [changeElement]
+        }];
+
+        expectedElements.push(changeElement);
+
+        assertModelHasElements(gridModel, frame, expectedElements, changes);
+      });
+
+      it('should not permanetly apply changes to the model', function () {
+        var expectedElements = makeEdits(gridModel);
+        var changeElement = { x: 3, y: 3, color: '#7777777' };
+        var changes = [{
+          action: GridModel.MODEL_ACTIONS.SET,
+          elements: [changeElement]
+        }];
+
+        gridModel.getElements(frame, changes);
+        assertModelHasElements(gridModel, frame, expectedElements);
+      });
+    });
+  });
+});
