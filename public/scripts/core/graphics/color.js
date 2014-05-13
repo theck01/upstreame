52a876f029cbb6f7c5a3107fe12d2760f2076451
Tyler Heck <tyler.heck@gmail.com>
2014-05-12 22:23:54 -0400

    add additional methods to Color

--- public/scripts/core/graphics/color.js
+++ public/scripts/core/graphics/color.js
@@ -1,73 +1,106 @@
-define([], function () {
+define(['underscore'], function (_) {
 
   // Color object contains a set of functions for dealing with color tuples
   // (objects with red, green, and blue fields)
   var Color = {};
 
+  
+  // _DEFAULT_COLOR is the default value for the improperly formatted color
+  // strings to take when provided to Color functions.
+  Color._DEFAULT_COLOR = '#FFFFFF';
+
+
+  // _DEFAULT_COLOR_COMPONENT is the default value for unspecified or bad color
+  // components in objects that should contain 'red', 'green', and 'blue'
+  // fields.
+  Color._DEFAULT_COLOR_COMPONENT = 255;
+
   // sanitize takes a color hexadecimal string and returns a properly formatted
   // color hexadecimal string
   //
   // Arguments:
-  //   color: A hexidecimal color string in the form "#RRGGBB"
+  //   color: A hexidecimal color string in the form '#RRGGBB'
   //
   // Returns:
-  //   A hexidecimal color string in the form "#RRGGBB", "#FFFFFF" if the
+  //   A hexidecimal color string in the form '#RRGGBB', '#FFFFFF' if the
   //   color is improperly formatted
   Color.sanitize = function (color) {
     var sanitizedColor = null;
 
-    if(color[0] !== "#"){
-      color = "#" + color;
+    if(color[0] !== '#'){
+      color = '#' + color;
     }
 
-    // check for format "#RRGGBB" and "#RGB"
+    // check for format '#RRGGBB' and '#RGB'
     if(color.match(/^#[A-Fa-f0-9]{6}$/g)) return color.toUpperCase();
     else if(color.match(/^#[A-Fa-f0-9]{3}$/g)){
-      sanitizedColor = "#" + color[1] + color[1] + color[2] + color[2];
+      sanitizedColor = '#' + color[1] + color[1] + color[2] + color[2];
       sanitizedColor += color[3] + color[3];
       return sanitizedColor.toUpperCase();
     }
 
-    return "#FFFFFF";
+    return Color._DEFAULT_COLOR;
   };
-
-
-  // shade a single component of a hexadecimal color
-  //
-  // Arguments:
-  //   color: A two digit hexadecimal string
-  //   change: An integer value to offset each RGB field by. Negative changes
-  //           darken colors, positive changes brighten them
-  //
-  // Returns: A two digit hexadecimal string
-  function shadeComponent (comp, change) {
-    comp = parseInt(comp, 16) + change;
-    comp = Math.max(Math.min(comp, 255), 0).toString(16);
-    return ("0" + comp).slice(-2);
-  }
   
+
   // shade brightens or darkens a hexadecimal color string
   //
   // Arguments:
-  //   color: A hexidecimal color string in the form "#RRGGBB"
+  //   color: A hexidecimal color string in the form '#RRGGBB'
   //   change: An integer value to offset each RGB field by. Negative changes
   //           darken colors, positive changes brighten them
   //
-  // Returns: A hexidecimal color string in the form "#RRGGBB"
+  // Returns: A hexidecimal color string in the form '#RRGGBB'
   Color.shade = function (color, change) {
-    color = Color.sanitize(color);
+    var colorObj = Color.toObject(color);
+    
+    _.each(['red', 'green', 'blue'], function (comp) {
+      colorObj[comp] += change;
+    });
 
-    var red = color.substr(1,2);
-    var green = color.substr(3,2);
-    var blue = color.substr(5,2);
-
-    var shadedColor = "#";
-    shadedColor += shadeComponent(red, change);
-    shadedColor += shadeComponent(green, change);
-    shadedColor += shadeComponent(blue, change);
-
-    return Color.sanitize(shadedColor);
+    return Color.toString(colorObj);
   };
+
+
+  // toObject converts a color hexadecimal string to an object with 'red',
+  // 'green', and 'blue' fields.
+  //
+  // Arguments:
+  //   color: A hexadecimal color string in the form '#RRGGBB'
+  //   opt_skipSanitation: Optional boolean on whether to skip sanitization.
+  //                       Can be set to true if colors have already been
+  //                       sanitized previously. Defaults to false.
+  // Returns an obje with 'red', 'green', and 'blue' fields
+  Color.toObject = function (color, opt_skipSanitation) {
+    var colorObj = Object.create(null);
+
+    color = opt_skipSanitation ? color : Color.sanitize(color);
+    colorObj.red = parseInt(color.substr(1,2), 16);
+    colorObj.green = parseInt(color.substr(3,2), 16);
+    colorObj.blue = parseInt(color.substr(5,2), 16);
+
+    return colorObj;
+  };
+
+
+  // toString converts a color object with 'red', 'green', and 'blue' integer
+  // fields to a hexadecimal string in the form '#RRGGBB'
+  //
+  // Arguments:
+  //   colorObj: An object with 'red', 'green', 'blue' fields.
+  // Returns a hexadecimal color string in the form '#RRGGBB'
+  Color.toString = function (colorObj) {
+    var color = '#';
+
+    _.each(['red', 'green', 'blue'], function (comp) {
+      var component = colorObj[comp] || Color._DEFAULT_COLOR_COMPONENT;
+      var compStr = Math.max(Math.min(component, 255), 0).toString(16);
+      color += ('0' + compStr).slice(-2).toUpperCase();
+    });
+
+    return color;
+  };
+
 
   return Color;
 });
--- spec/public/core/graphics/color.spec.js
+++ spec/public/core/graphics/color.spec.js
@@ -1,4 +1,3 @@
-var assert = require('assert');
 var requirejs = require('requirejs');
 var should = require('should');
 
@@ -10,31 +9,87 @@ requirejs.config({
   }
 });
 
+function assertColorObjsEqual (colorObj1, colorObj2) {
+  colorObj1.red.should.eql(colorObj2.red);
+  colorObj1.green.should.eql(colorObj2.green);
+  colorObj1.blue.should.eql(colorObj2.blue);
+}
+
 var Color = requirejs('core/graphics/color');
 
 describe('Color', function () {
-  context('when sanitize is provided reasonably formed color strings',
+  context('when functions are provided reasonably formed color strings',
     function () {
-      it('should sanitize those strings into form "#RRGGBB"', function () {
-        Color.sanitize('#123456').should.eql('#123456');
-        Color.sanitize('123456').should.eql('#123456');
-        Color.sanitize('#123').should.eql('#112233');
-        Color.sanitize('123').should.eql('#112233');
-      });
+      it('sanitize should return equivalent strings in "#RRGGBB" form',
+        function () {
+          Color.sanitize('#123456').should.eql('#123456');
+          Color.sanitize('123456').should.eql('#123456');
+          Color.sanitize('#123').should.eql('#112233');
+          Color.sanitize('123').should.eql('#112233');
+        }
+      );
+
+      it('toObject should return an equivalent object split into components',
+        function () {
+          var expectedObj = { red: 255, green: 17, blue: 34 };
+          assertColorObjsEqual(Color.toObject('#FF1122'), expectedObj);
+          assertColorObjsEqual(Color.toObject('fF1122'), expectedObj);
+          assertColorObjsEqual(Color.toObject('#f12'), expectedObj);
+          assertColorObjsEqual(Color.toObject('F12'), expectedObj);
+        }
+      );
+
+      it('shade should return a color string with the appropriate offset',
+        function () {
+          Color.shade('#111111', -1).should.eql('#101010');
+          Color.shade('#A0A0A0', 10).should.eql('#AAAAAA');
+        }
+      );
     }
   );
 
-  context('when sanitize is provided poorly formed color strings', function () {
-    it('should return a default color "#FFFFFF"', function () {
-      Color.sanitize('').should.eql('#FFFFFF');
-      Color.sanitize('#00').should.eql('#FFFFFF');
-      Color.sanitize('#12345G').should.eql('#FFFFFF');
-      Color.sanitize('#1234567').should.eql('#FFFFFF');
-    });
-  });
+  context('when functions are provided poorly formed color strings',
+    function () {
+      it('sanitize should return a default color "#FFFFFF"', function () {
+        Color.sanitize('').should.eql('#FFFFFF');
+        Color.sanitize('#00').should.eql('#FFFFFF');
+        Color.sanitize('#12345G').should.eql('#FFFFFF');
+        Color.sanitize('#1234567').should.eql('#FFFFFF');
+      });
 
-  it('should shade colors with the appropriate offset', function () {
-    Color.shade('#111111', -1).should.eql('#101010');
-    Color.shade('#A0A0A0', 10).should.eql('#AAAAAA');
-  });
+      it('toObject should return an object equivalent to "#FFFFFF"',
+        function () {
+          var expectedObj = { red: 255, green: 255, blue: 255 };
+          assertColorObjsEqual(Color.toObject(''), expectedObj);
+          assertColorObjsEqual(Color.toObject('F1122'), expectedObj);
+          assertColorObjsEqual(Color.toObject('#fg1213'), expectedObj);
+          assertColorObjsEqual(Color.toObject('#FF12012'), expectedObj);
+        }
+      );
+    }
+  );
+
+  context('when functions are passed properly formatted color objects',
+    function () {
+      it('toString should return an equivalent "#RRGGBB" color string',
+        function () {
+          Color.toString({ red: 255, blue: 34, green: 17 }).should
+            .eql('#FF1122');
+          Color.toString({ red: 1024, blue: -29485, green: 17 }).should
+            .eql('#FF1100');
+        }
+      );
+    }
+  );
+
+  context('when functions are passed color objects missing fields',
+    function () {
+      it('toString should return an equivalent "#RRGGBB" color string while' +
+         'filling missing fields with a default value', function () {
+          Color.toString({ red: 255, green: 17 }).should
+            .eql('#FF11FF');
+        }
+      );
+    }
+  );
 });
