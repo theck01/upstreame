afde475cf11ceeaa94365b1ada073a7f87028e11
Tyler Heck <tyler.heck@gmail.com>
2014-06-29 18:27:35 -0400

    initialize /pixeleditor toolbar buttons and radio group

--- public/scripts/core/pixeleditor.js
+++ public/scripts/core/pixeleditor.js
@@ -11,13 +11,45 @@ require.config({
   }
 });
 
+
 require(
-    ['jquery', 'domkit/controllers/radiogroup', 'domkit/ui/button',
-     'domkit/ui/palette'],
-    function ($, RadioGroup, Button, Palette) {
+    ['jquery', 'underscore', 'domkit/controllers/radiogroup',
+     'domkit/ui/button', 'domkit/ui/palette'],
+    function ($, _, RadioGroup, Button, Palette) {
+
+  // initializeButtons initializes all domkit buttons and returns button
+  // instances in an object with buttons namespaced to location in the view.
+  function initializeButtons() {
+    var buttons = Object.create(null);
+
+    buttons.toolbar = Object.create(null);
+    buttons.toolbar.activeColor = Button.create('#active-color-button');
+    buttons.toolbar.defaultColor = Button.create('#default-color-button');
+    buttons.toolbar.toolSelect = Button.create('#tool-select-button');
+    buttons.toolbar.undo = Button.create('#undo-button');
+    buttons.toolbar.redo = Button.create('#redo-button');
+    buttons.toolbar.zoom = Button.create('#zoom-button');
+    buttons.toolbar.trash = Button.create('#trash-button');
+    buttons.toolbar.load = Button.create('#load-button');
+    buttons.toolbar.save = Button.create('#save-button');
+    buttons.toolbar.settings = Button.create('#settings-button');
+    buttons.toolbar.session = Button.create('#session-button');
+
+    return buttons;
+  }
+
+
+  // onload -- main
   $(function () {
-    window.RadioGroup = RadioGroup;
-    window.Button = Button;
-    window.Palette = Palette;
+    window.buttons = initializeButtons();
+    window.toolbarRadioGroup = new RadioGroup(_.values(_.omit(
+        window.buttons.toolbar, ['undo', 'redo'])));
+
+    $(document).bind('keydown', function (e) {
+      // If the escape key was pressed clear toolbar selection.
+      if (e.which === 27) {
+        window.toolbarRadioGroup.clear();
+      }
+    });
   });
 });
