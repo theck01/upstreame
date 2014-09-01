2e8381c7643ea00d94f940c8f55e875c69295f7c
Tyler Heck <tyler.heck@gmail.com>
2014-09-01 12:10:20 -0400

    add buildfile for blog page

---
+++ build/blog.js
@@ -0,0 +1,9 @@
+({
+  baseUrl: "../public/scripts",
+  paths: {
+    jquery: "../../bower_components/jquery/jquery.min",
+    underscore: "../../bower_components/underscore-amd/underscore-min"
+  },
+  name: "blog/main",
+  out: "../public/scripts/dist/blog.min.js"
+})
