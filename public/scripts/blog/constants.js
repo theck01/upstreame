645bc1cdf071c05830b6a795a7f3ea463699b722
Tyler Heck <tyler.heck@gmail.com>
2014-09-07 18:38:29 -0400

    sharpen up /posts/* styling

--- public/scripts/blog/constants.js
+++ public/scripts/blog/constants.js
@@ -2,8 +2,8 @@ define([], function () {
   // Singleton containing all constants for the Blog application.
   var Constants = Object.create(null);
 
-  Constants.LOGO_CANVAS_DIMENSIONS = { width: 80, height: 80 };
-  Constants.LOGO_CANVAS_OFFSET = { x: 40, y: 40 };
+  Constants.LOGO_CANVAS_DIMENSIONS = { width: 64, height: 64 };
+  Constants.LOGO_CANVAS_OFFSET = { x: 32, y: 32 };
 
   return Constants;
 });
